a5c8acbe824c25f49bc519c8177507f484880b17
sanat <sanatrathore1214@gmail.com>
2023-02-22 14:46:41 +0530

    two filter methods use

--- src/Map/StreamConcepts.java
+++ src/Map/StreamConcepts.java
@@ -3,7 +3,9 @@ package Map;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamConcepts {
 
@@ -32,6 +34,27 @@ public class StreamConcepts {
 		Optional<Employee> max = list.stream().max(Comparator.comparing(Employee::getSalary));
 		
 		System.out.println("maximum salary is :"+max.get().getSalary());
+		
+		
+		// name of the employee having salary greater than 300
+		List<String> empName = list.stream().filter(emp -> emp.getSalary()>300).map(emp -> emp.getFirstName()).collect(Collectors.toList());
+		System.out.println(empName);
+		
+		
+		//filter name which starts with s and length greater than 3
+		list.stream().filter(emp -> emp.getFirstName().startsWith("s")).filter(emp -> emp.getFirstName().length()>3).map(emp -> emp.getFirstName()).forEach(System.out::println);
+		
+		// another way
+		Predicate<Employee> p1 = e -> e.getFirstName().startsWith("s");
+		
+		Predicate<Employee> p2 = e -> e.getFirstName().length() >3;
+		
+		list.stream().filter(p1.and(p2)).map(emp -> emp.getFirstName()).forEach(System.out::println);
+		
+		
+		
+		
+		
 		
 	}
 
